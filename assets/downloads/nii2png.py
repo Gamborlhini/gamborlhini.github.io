20096aae36b93641cc6fbcbbc665a7d1266f5196
Gamborlhini <nikhil.subhas@gmail.com>
2024-06-05 17:08:48 -0400

    tool to convert nifti to png slices

---
+++ assets/downloads/nii2png.py
@@ -0,0 +1,44 @@
+import nibabel as nib
+import argparse
+import glob
+import os
+import cv2
+import numpy as np
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-i', '--input', type=str)
+parser.add_argument('-o', '--output', type=str)
+args = parser.parse_args()
+
+nii_folder = args.input
+output_folder = args.output
+
+try:
+    if not os.path.exists(nii_folder):
+        raise FileNotFoundError("input folder not found")
+    if not os.path.exists(output_folder):
+        raise FileNotFoundError("output folder not found")
+except FileNotFoundError:
+    print(FileNotFoundError)
+    raise
+
+NiiCaseLst = sorted(glob.glob(os.path.join(nii_folder, "*.nii.gz")))
+print(NiiCaseLst)
+
+for case in NiiCaseLst:
+    img_data = nib.load(case)
+    img_data = img_data.get_fdata()
+    img_data_unsigned = img_data - np.min(img_data)
+
+    img_data_uint16 = img_data_unsigned.astype(np.uint16)
+
+    for i in range(img_data_uint16.shape[..., 0]):
+        out_folder_prefix = case.split('_')[0]
+        case_output_folder = os.path.join(output_folder, out_folder_prefix)
+        os.mkdir(case_output_folder)
+        out_path = str(os.path.join(case_output_folder, "T" + str(i+1).zfill(3) + ".png"))
+
+        img_uint16_slice = img_data_uint16[..., i]
+        img = np.repeat(img_uint16_slice, 3, axis=-1)
+        cv2.imwrite(out_path, img)
+        print(out_folder_prefix + "\n\n")
