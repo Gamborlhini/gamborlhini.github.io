10cf14273a5c1b5daa60938ba86a5cfad0853f6a
Gamborlhini <nikhil.subhas@gmail.com>
2024-06-18 11:44:25 -0400

    Updated threshold
    Reads 3D

--- assets/research/threshold.py
+++ assets/research/threshold.py
@@ -2,7 +2,7 @@
 Created on 12/06/2024
 @author: Nikhil Subhas
 
-script to threshold .png image stacks from CT scans and apply a mask
+script to threshold .png image stacks from CT scans and apply a decode_mask
 
 args.input expects an input directory with subdirectories containing .png image stacks
 
@@ -53,10 +53,10 @@ except FileNotFoundError:
 MaskLst = sorted(glob.glob(masks_folder + "/*"))
 ImageLst = sorted(glob.glob(input_folder + "/*"))
 
-# iterate through each mask subdirectory
+# iterate through each decode_mask subdirectory
 for idx1, mask in MaskLst:
 
-    # validate that the mask subdirectory has a matching image subdirectory
+    # validate that the decode_mask subdirectory has a matching image subdirectory
     try:
         if os.path.basename(MaskLst[idx1]) != os.path.basename(ImageLst[idx1]):
             raise IndexError("Case numbers do not match for masks and cases")
@@ -65,46 +65,35 @@ for idx1, mask in MaskLst:
         raise
 
     # select all the slices of the input image
-    sliceLst = sorted(glob.glob(os.path.join(input_folder, ImageLst[idx1], "*.png")))
+    sliceLst = sorted(glob.glob(os.path.join(ImageLst[idx1], "*.png")))
 
-    # each mask should be in the shape n x m x slices
-    full_mask = np.load(os.path.join(masks_folder, MaskLst[idx1], "full_mask.npy"))
-    infraspinatus_mask = np.load(os.path.join(masks_folder, MaskLst[idx1], "infraspinatus_mask.npy"))
-    supraspinatus_mask = np.load(os.path.join(masks_folder, MaskLst[idx1], 'supraspinatus_mask.npy'))
-    subscapularis_mask = np.load(os.path.join(masks_folder, MaskLst[idx1], 'subscapularis_mask.npy'))
-
-    # initialize empty fat masks
-    full_mask_fat, infraspinatus_mask_fat, supraspinatus_mask_fat, subscapularis_mask_fat = [], [], [], []
+    # initialize decode_mask and image
+    full_mask = np.load(os.path.join(MaskLst[idx1], "full_mask.npy"))
+    image = np.zeros_like(full_mask)
 
     for idx2, filenameImg in sliceLst:
         # img should be n x m x 3 which we cut down to n x m since the three channels
         # should be identical
-        img, img_ori = utils.read_image_one_hot(filenameImg, dtype=tf.dtypes.uint16)
+        img, img_ori = utils.read_image_one_hot(filenameImg, dtype=tf.dtypes.uint16)[0]
+        image[idx2] = img_ori
 
-        # ensure the slice of image and slice of mask have the same size
-        try:
-            if full_mask[..., idx2].shape != img_ori.shape:
-                raise TypeError("Mask and slice are not the same size")
-        except TypeError:
-            print(TypeError)
-            raise
+        # ensure the image and decode_mask have the same size
+    try:
+        if image.shape != full_mask.shape:
+            raise TypeError("Mask and slice are not the same size")
+    except TypeError:
+        print(TypeError)
+        raise
 
-        # create a boolean array of when the image value is smaller than the upper threshold
-        fat = (img < upperThreshold)
-
-        full_mask_fat = full_mask[fat, idx2]
-        # combine that array with the segmentation masks using a logical and function
-        infraspinatus_mask_fat[:, :, idx2] = np.logical_and(fat, infraspinatus_mask.astype(bool))
-        supraspinatus_mask_fat[:, :, idx2] = np.logical_and(fat, supraspinatus_mask.astype(bool))
-        subscapularis_mask_fat[:, :, idx2] = np.logical_and(fat, subscapularis_mask.astype(bool))
+    # create uint16 array of when the image value is smaller than the upper threshold (0 or 1)
+    fat = np.array(image < upperThreshold).astype(np.uint16)
+    full_mask_fat = np.add(fat, full_mask)
+    full_mask_fat[full_mask_fat <= 1] = 0
+    full_mask_fat[full_mask_fat == 2] = 1
+    full_mask_fat[full_mask_fat == 3] = 2
+    full_mask_fat[full_mask_fat == 4] = 3
 
     # save all the fat masks to the output folder within subdirectories
     if args.output:
         np.save(os.path.join(str(output_folder), os.path.basename(MaskLst[idx1]), "full_mask_fat.npy"),
                 full_mask_fat)
-        np.save(os.path.join(str(output_folder), os.path.basename(MaskLst[idx1]), "infraspinatus_mask_fat.npy"),
-                infraspinatus_mask_fat)
-        np.save(os.path.join(str(output_folder), os.path.basename(MaskLst[idx1]), "supraspinatus_mask_fat.npy"),
-                supraspinatus_mask_fat)
-        np.save(os.path.join(str(output_folder), os.path.basename(MaskLst[idx1]), "subscapularis_mask_fat.npy"),
-                subscapularis_mask_fat)
